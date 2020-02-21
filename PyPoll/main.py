af1039abd8f503971793d2f97b76bb5aa7706321
Robert Ottogalli <59754310+Rob-Ottogalli@users.noreply.github.com>
2020-02-20 21:57:28 -0600

    Added CSV reader and total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,4 +2,28 @@ import os
 import csv
 
 # Path to collect data from the Resources folder
-election_csv = os.path.join('Resources', 'election_data.csv')
+csvpath = os.path.join('Resources', 'election_data.csv')
+
+# Initialize variables to hold data
+Total_Votes = 0
+Candidates = []
+
+
+
+# Read CSV
+with open(csvpath, newline="") as csvfile:
+    election_csv = csv.reader(csvfile, delimiter=",")
+    # skip headers
+    header = next(election_csv)
+
+    # Loop through data 
+    for row in election_csv:
+        # Find total number of votes
+        if row[0] != 0:
+            Total_Votes = Total_Votes + 1
+
+    # Loop through data
+    # for row in election_csv:
+        # find discrete candidates
+
+    print(Total_Votes)
