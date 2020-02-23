06343f7f43411b27bd2c4a40c25eae22f1589c4c
Robert Ottogalli <59754310+Rob-Ottogalli@users.noreply.github.com>
2020-02-22 19:54:19 -0600

    Added CSV reader inside Vote Tally function.  Function now counts votes for all candiates after each loop

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,16 +11,23 @@ Final_Count = {}
 Candidate_Votes = 0
 
 # Vote Tally function to count the number of votes for a single candidate
+# "candiate" in the input of the function will be the candidate's name
 def Vote_Tally(candidate):
-    Candidate_Votes = 0             # Initialize variable for number of candidate votes
-    # candidate = Cand       # Set candidate to be equal to candidate in list.  Applicable in a for loop.
-    for row in election_csv:        # Search for candidate's number of occurrences in the CSV. 
-        if row[2] == candidate:         # Increase the # of candidate votes if candidates' name is found
-            Candidate_Votes = Candidate_Votes + 1
-    y = Candidate_Votes             # Set value for dictionary
-    Final_Count.update([(candidate, y)])    # Append key:value to dictionary, where candidate's name is the key and candidate votes is the value
-    return Final_Count
-    # Candidate_Votes = 0
+    # Read CSV
+    with open(csvpath, newline="") as csvfile:
+        election_csv = csv.reader(csvfile, delimiter=",")
+        # skip headers
+        header = next(election_csv)
+
+        # Count number of candidate votes
+        Candidate_Votes = 0                             # Initialize variable for number of candidate votes
+        for row in election_csv:                        # Loop through rows of CSV.
+            if row[2] == candidate:                     # Search for candidate's name in each row. 
+                Candidate_Votes = Candidate_Votes + 1   # Increase the # of candidate votes if candidates' name is found.
+        y = Candidate_Votes                             # Set value for dictionary to # of occurrences of candidate's name
+        Final_Count.update([(candidate, y)])            # Append key:value to dictionary, where candidate's name is the key and candidate votes is the value
+        return Final_Count                              # Return updated dictionary, with new candidate/vote counts added
+
 
 # Read CSV
 with open(csvpath, newline="") as csvfile:
@@ -38,39 +45,21 @@ with open(csvpath, newline="") as csvfile:
         if str(row[2]) not in Candidates:
             Candidates.append(str(row[2])) 
 
-   # Find number of Candidates
+    # Find number of Candidates
     Num_Cand = len(Candidates)
 
+    # Count votes for each candidate.  Loop through each candidate
+    for x in range(0, Num_Cand):
+        Cand = Candidates[x]
+        count = Vote_Tally(Cand)
     # print(Vote_Tally(Candidates[0]))
 
 # for x in range(0, Num_Cand):
 #     Cand = Candidates[x]
 
-# Read CSV
-with open(csvpath, newline="") as csvfile:
-    election_csv = csv.reader(csvfile, delimiter=",")
-
-    # Code works - finds correct count
-    # for x in range(0, Num_Cand):
-    #     for row in election_csv:
-    #         if row[2] == Cand:
-    #             Candidate_Votes = Candidate_Votes + 1
-    #             j = Candidates[x]
-    #             y = Candidate_Votes
-    #             Final_Count.update(j = y)
-    
-    # Count votes for each candidate.  Loop through each candidate
-    for x in range(0, Num_Cand):
-        Cand = Candidates[x]
-        count = Vote_Tally(Cand)
-       
         # Final_Count.update( = count)
         # y = Candidate_Votes             # Set value for dictionary
         # Final_Count.update([(candidate, count)])    # Append key:value to dictionary, where candidate's name is the key and candidate votes is the value
-
-    # Khan = Vote_Tally("Khan")
-    # Correy = Vote_Tally("Correy")
-    # Li = Vote_Tally("Li")
 
 
 ### NOTE:  The function above only calculates for Candidate 0.  Need to debug to get it through the
